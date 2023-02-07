8bb8606cbe3aa01002fc2bf96c271836cfac62ee
Enrique Marin <enrique_marin_h@hotmail.com>
2023-02-07 13:41:45 -0700

    Fix console.log in app.listen

--- src/server.js
+++ src/server.js
@@ -31,5 +31,5 @@ setInterval(function () {
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log('Server is running on port: ${port}');
+  console.log(`Server is running on port: ${port}`);
 });
