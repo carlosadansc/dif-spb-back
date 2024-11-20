76b25bbc6b5d64c6da42d02cc1ddc8dc852e2727
carlosadansc <charliecesena.94@gmail.com>
2024-11-20 13:18:19 -0700

    Change contribution model and controller

--- src/controllers/contribution.controller.js
+++ src/controllers/contribution.controller.js
@@ -6,41 +6,72 @@ const httpStatus = require("../common/HttpStatusCodes");
 const errorCode = require("../common/ErroCodes");
 
 // CREATE contribution
-exports.create = async (req, res) => {
-  const createdBy = tokenUtils.decodeToken(req.headers["authorization"]).id;
-  const currentuser = tokenUtils.decodeToken( req.headers["authorization"] ).username;
-  const { contributionItem, beneficiary, applicant, quantity, comments } = req.body;
+// exports.create = async (req, res) => {
+//   const createdBy = tokenUtils.decodeToken(req.headers["authorization"]).id;
+//   const currentuser = tokenUtils.decodeToken( req.headers["authorization"] ).username;
+//   const { contributionItems, beneficiary, comments, contributionDate } = req.body;
 
-  const contribution = new Contribution({
-    contributionItem,
-    beneficiary,
-    applicant,
-    quantity,
-    comments,
-    createdBy,
-    createdAt: new Date(),
-  });
-  await contribution
-    .save()
-    .then(async () => {
-      // **** LOG **** //
-      const foundBeneficiary = await Beneficiary.findById(beneficiary);
-      foundBeneficiary.contributions.push(contribution._id);
-      await Beneficiary.findOneAndUpdate(
-        { _id: beneficiary },
-        { contributions: foundBeneficiary.contributions }
-      );
-      logger.log("POST", "/contribution/create", currentuser);
-      res.status(httpStatus.OK).json({ data: contribution, errors: [] });
-    })
-    .catch((err) => {
-      // **** LOG **** //
-      logger.log("POST", "/contribution/create", currentuser, err, false);
-      return res
-        .status(httpStatus.UNAUTHORIZED)
-        .send({ data: {}, errors: [errorCode.ERR0000] });
+//   const contribution = new Contribution({
+//     contributionItems,
+//     beneficiary,
+//     comments,
+//     contributionDate,
+//     createdBy,
+//     createdAt: new Date(),
+//   });
+//   await contribution
+//     .save()
+//     .then(async () => {
+//       // **** LOG **** //
+//       const foundBeneficiary = await Beneficiary.findById(beneficiary);
+//       foundBeneficiary.contributions.push(contribution._id);
+//       await Beneficiary.findOneAndUpdate(
+//         { _id: beneficiary },
+//         { contributions: foundBeneficiary.contributions }
+//       );
+//       logger.log("POST", "/contribution/create", currentuser);
+//       res.status(httpStatus.OK).json({ data: contribution, errors: [] });
+//     })
+//     .catch((err) => {
+//       // **** LOG **** //
+//       logger.log("POST", "/contribution/create", currentuser, err, false);
+//       return res
+//         .status(httpStatus.INTERNAL_SERVER_ERROR)
+//         .send({ data: {}, errors: [errorCode.ERR0000] });
+//     });
+// };
+
+exports.create = async (req, res) => {
+  try {
+    const { authorization } = req.headers;
+    const { id: createdBy, username: currentuser } = tokenUtils.decodeToken(authorization);
+    const { contributionItems, beneficiary, comments, contributionDate } = req.body;
+
+    const contribution = new Contribution({
+      contributionItems,
+      beneficiary,
+      comments,
+      contributionDate,
+      createdBy,
+      createdAt: new Date(),
     });
+
+    await contribution.save();
+
+    // **** LOG **** //
+    const foundBeneficiary = await Beneficiary.findById(beneficiary);
+    foundBeneficiary.contributions.push(contribution._id);
+    await Beneficiary.findByIdAndUpdate(beneficiary, { contributions: foundBeneficiary.contributions });
+
+    logger.log("POST", "/contribution/create", currentuser);
+    res.status(httpStatus.OK).json({ data: contribution, errors: [] });
+  } catch (err) {
+    // **** LOG **** //
+    logger.log("POST", "/contribution/create", currentuser, err, false);
+    res.status(httpStatus.INTERNAL_SERVER_ERROR).send({ data: {}, errors: [errorCode.ERR0000] });
+  }
 };
+
 
 
 // GET contribution by Beneficiary
--- src/models/contribution.model.js
+++ src/models/contribution.model.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 
 const ContributionSchema = new mongoose.Schema({
-  contributionItem: { type: mongoose.Schema.Types.ObjectId, ref: "ContributionItem", autopopulate: true, },
+  contributionItems: [{ 
+    contributionItem : {type: mongoose.Schema.Types.ObjectId, ref: "ContributionItem", autopopulate: true}, 
+    quantity: {type: Number, required: true},
+  }],
   beneficiary: { type: mongoose.Schema.Types.ObjectId, ref: "Beneficiary" },
-  applicant: { type: String, required: true },
-  quantity: { type: Number, required: true },
   comments: { type: String, required: false },
+  contributionDate: { type: Date, required: true, default: Date.now },
 
   active: { type: Boolean, required: true, default: true },
   deleted: { type: Boolean, required: true, default: false },
