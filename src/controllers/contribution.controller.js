df18cba6cf06026aed153593a5740c215e1c5a70
carlosadansc <charliecesena.94@gmail.com>
2024-11-21 10:56:17 -0700

    add endpoint getContributionsWhitCategory

--- src/controllers/contribution.controller.js
+++ src/controllers/contribution.controller.js
@@ -1,4 +1,5 @@
 const Contribution = require("../models/contribution.model");
+const ContributionItem = require("../models/contribution_item.model");
 const Beneficiary = require("../models/beneficiary.model");
 const tokenUtils = require("../utils/TokenUtils");
 const logger = require("../utils/Logger");
@@ -44,8 +45,10 @@ const errorCode = require("../common/ErroCodes");
 exports.create = async (req, res) => {
   try {
     const { authorization } = req.headers;
-    const { id: createdBy, username: currentuser } = tokenUtils.decodeToken(authorization);
-    const { contributionItems, beneficiary, comments, contributionDate } = req.body;
+    const { id: createdBy, username: currentuser } =
+      tokenUtils.decodeToken(authorization);
+    const { contributionItems, beneficiary, comments, contributionDate } =
+      req.body;
 
     const contribution = new Contribution({
       contributionItems,
@@ -61,23 +64,25 @@ exports.create = async (req, res) => {
     // **** LOG **** //
     const foundBeneficiary = await Beneficiary.findById(beneficiary);
     foundBeneficiary.contributions.push(contribution._id);
-    await Beneficiary.findByIdAndUpdate(beneficiary, { contributions: foundBeneficiary.contributions });
+    await Beneficiary.findByIdAndUpdate(beneficiary, {
+      contributions: foundBeneficiary.contributions,
+    });
 
     logger.log("POST", "/contribution/create", currentuser);
     res.status(httpStatus.OK).json({ data: contribution, errors: [] });
   } catch (err) {
     // **** LOG **** //
     logger.log("POST", "/contribution/create", currentuser, err, false);
-    res.status(httpStatus.INTERNAL_SERVER_ERROR).send({ data: {}, errors: [errorCode.ERR0000] });
+    res
+      .status(httpStatus.INTERNAL_SERVER_ERROR)
+      .send({ data: {}, errors: [errorCode.ERR0000] });
   }
 };
-
-
 
 // GET contribution by Beneficiary
 exports.getContributionByBeneficiary = (req, res) => {
   const { beneficiary } = req.params;
-  const currentuser = tokenUtils.decodeToken( req.headers["authorization"] ).username;
+  const currentuser = tokenUtils.decodeToken( req.headers["authorization"]).username;
   Contribution.find({ beneficiary: beneficiary })
     .then((contributions) => {
       // **** LOG **** //
@@ -87,8 +92,41 @@ exports.getContributionByBeneficiary = (req, res) => {
     .catch((err) => {
       // **** LOG **** //
       logger.log("GET", `/contribution/${beneficiary}`, currentuser, err, false);
-      return res
-        .status(httpStatus.INTERNAL_SERVER_ERROR)
-        .send({ data: {}, errors: [errorCode.ERR0000, err] });
+      return res.status(httpStatus.INTERNAL_SERVER_ERROR).send({ data: {}, errors: [errorCode.ERR0000, err] });
     });
+};
+
+// GET all contributions with catergory
+exports.getContributionsWhitCategory = async (req, res) => {
+  const currentuser = tokenUtils.decodeToken( req.headers["authorization"]).username;
+  try {
+    const contributions = await Contribution.aggregate([
+      { $unwind: "$contributionItems" },
+      {
+        $lookup: {
+          from: "contributionitems",
+          localField: "contributionItems.contributionItem",
+          foreignField: "_id",
+          as: "contributionItemDetails",
+        },
+      },
+      { $unwind: "$contributionItemDetails" },
+      {
+        $group: {
+          _id: "$contributionItemDetails.category",
+          totalQuantity: { $sum: "$contributionItems.quantity" },
+          contributions: { $push: "$$ROOT" },
+        },
+      },
+      { $sort: { _id: 1 } },
+    ]);
+
+     // **** LOG **** //
+     logger.log("GET", `/contribution/categories`, currentuser);
+     res.status(httpStatus.OK).send({ data: contributions, errors: [] });
+  } catch (err) {
+     // **** LOG **** //
+     logger.log("GET", `/contribution/categories`, currentuser, err, false);
+     return res.status(httpStatus.INTERNAL_SERVER_ERROR).send({ data: {}, errors: [errorCode.ERR0000, err] });
+  }
 };
--- src/routes/contribution.routes.js
+++ src/routes/contribution.routes.js
@@ -5,4 +5,7 @@ const UserMiddleware = require("../middlewares/UserMiddleware")
 // CREATE contribution
 router.post("/contribution/create", UserMiddleware.verifyUser, ContributionController.create);
 
+// GET contribution whit category
+router.get("/contribution/categories", UserMiddleware.verifyUser, ContributionController.getContributionsWhitCategory);
+
 module.exports = router;
